8b873811f2f24200d523ae7df346cf863dca30a4
NlengDominique <luqnleng5@gmail.com>
2025-07-22 12:40:57 +0100

    ajout des relations entre models

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -14,4 +14,10 @@ class Task extends Model
         'status',
         'user_id'
     ];
+
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
