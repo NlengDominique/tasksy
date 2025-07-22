8fdcd4ddd112d359605e6cbb0f6d250cd152f100
NlengDominique <luqnleng5@gmail.com>
2025-07-22 12:38:06 +0100

    creation du schema de la base de donnees

--- database/migrations/2025_07_22_111311_create_tasks_table.php
+++ database/migrations/2025_07_22_111311_create_tasks_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->dateTime('dateEcheance')->nullable();
+            $table->enum('priority', ['faible', 'moyen', 'eleve'])->default('moyen');
+            $table->enum('status',['en attente','terminee'])->default('en attente');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
